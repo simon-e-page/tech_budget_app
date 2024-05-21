ccdd3de9928e3142370d38bb9764987d54e31ca2
Simon Page <simon.e.page@gmail.com>
2024-05-21 20:17:59 +1000

    Removed server calls

--- client_code/Data.py
+++ client_code/Data.py
@@ -90,7 +90,7 @@ class Budget(AttributeToDict):
 
   def save(self):
     try:
-      ret = anvil.server.call('save_budget_entries', fin_year=self.fin_year, entries=self.to_dict())
+      ret = {} #anvil.server.call('save_budget_entries', fin_year=self.fin_year, entries=self.to_dict())
     except Exception as e:
       print("Exception saving Budget Entries!")
       print(e)
@@ -165,23 +165,23 @@ class Account(AttributeToKey):
     # Saves to backend as new or updated object
     props = { x: self[x] for x in list(self._defaults.keys()) }
     print("Save Account")
-    anvil.server.call('add_account', props)    
+    #anvil.server.call('add_account', props)    
 
   def get_balance(self, as_date=None):
     if as_date is None:
-      balance = anvil.server.call('get_account_balance', self.name)
+      balance = 0.0 #anvil.server.call('get_account_balance', self.name)
     else:
-      balance = anvil.server.call('get_starting_balance', self.name, as_date)
+      balance = 0.0 #anvil.server.call('get_starting_balance', self.name, as_date)
     return balance
     
   def to_dict(self):
     return { x: self[x] for f in self._defaults }
 
   def is_fully_reconciled(self):
-    return anvil.server.call('is_fully_reconciled', self.name)
+    return True #anvil.server.call('is_fully_reconciled', self.name)
 
   def get_account_periods(self):
-    return anvil.server.call('get_account_periods', self.name)
+    return [] #anvil.server.call('get_account_periods', self.name)
 
 class Accounts(AttributeToDict):
   def __init__(self, account_list=None):
@@ -195,14 +195,14 @@ class Accounts(AttributeToDict):
     
   def match_filename(self, filename):
     # TODO: better to keep on frontend?
-    account_name = anvil.server.call('get_account_from_filename', filename)
+    account_name = None #anvil.server.call('get_account_from_filename', filename)
     return account_name
 
   def test_reconciled(self):
-    return anvil.server.call('test_reconciled')
+    return False #anvil.server.call('test_reconciled')
 
   def get_accounts_by_month(self, end, compare='budget', start=None, account_names=None, fill=True):
-    return anvil.server.call("get_accounts_by_month", end, compare, start=start, account_names=account_names, fill=fill)
+    return None #anvil.server.call("get_accounts_by_month", end, compare, start=start, account_names=account_names, fill=fill)
 
 
 
@@ -227,7 +227,7 @@ class Icon(AttributeToKey):
           alert("Selected file is too large. Please reduce and try again!")
         elif (content.content_type in ACCEPTABLE.keys()):
           name = name + "." + ACCEPTABLE[content.content_type]
-          icon_id = anvil.server.call('create_account_icon_file', name, content)
+          icon_id = None #anvil.server.call('create_account_icon_file', name, content)
         else:
           alert('Not an acceptable image file. Please try again!')
       except Exception as e:    
@@ -248,7 +248,7 @@ class Icons(AttributeToDict):
     return len(self.__d__)
 
   def load(self):
-    icons_cache = anvil.server.call('get_icons')
+    icons_cache = {} #anvil.server.call('get_icons')
     for k, v in icons_cache.items():
       self.add(k, Icon(name=v.name, content=v, icon_id=k))
 
@@ -298,7 +298,7 @@ class Rule(AttributeToKey):
     ret = None
     if self.id is None:
       try:
-        self.id = anvil.server.call('add_rule', self.pattern_description, self.account_name, **props)    
+        self.id = None #anvil.server.call('add_rule', self.pattern_description, self.account_name, **props)    
         ret = self.id
       except Exception as e:
         print("Error adding new rule!")
@@ -314,7 +314,8 @@ class Rule(AttributeToKey):
       
   def delete(self):
     try:
-      anvil.server.call('delete_rule', rule_id=self.id)
+      #anvil.server.call('delete_rule', rule_id=self.id)
+      pass
     except Exception as e:
       print("Error calling delete_rule with {0}".format(pattern))
 
@@ -671,7 +672,7 @@ class LazyTransactionList:
     """ Returns a set of matching transactions from the backend
         Operates independently from the set of cached items
     """
-    matched_trans_list = anvil.server.call('match_transactions', transaction.to_dict())
+    matched_trans_list = [] #anvil.server.call('match_transactions', transaction.to_dict())
     matched_trans = [ Transaction(transaction_json=x) for x in matched_trans_list ]
     return matched_trans
 
@@ -687,16 +688,16 @@ class LazyTransactionList:
 
 class Importer:
   def start(self, file_obj, account_name):
-    return anvil.server.call('upload_transactions', file_obj, account_name)
+    return None #anvil.server.call('upload_transactions', file_obj, account_name)
 
   def next_batch(self, code):
-    return anvil.server.call('upsert_batch', code)
+    return None #anvil.server.call('upsert_batch', code)
 
   def process_paypal(self, account_name, start, end):
-    return anvil.server.call('process_paypal', account_name, start=start, end=end)
+    return None #anvil.server.call('process_paypal', account_name, start=start, end=end)
 
   def get_import_ids(self, account_name):
-    return anvil.server.call('get_import_ids', account_name)
+    return [] #anvil.server.call('get_import_ids', account_name)
 
 
 ACCOUNTS_D = Accounts()
@@ -718,10 +719,10 @@ def get_transactions():
 
 def refresh():
   global ACCOUNTS_D, FIN_YEARS, IMPORTERS, ICONS
-  account_list = [ Account(account_json=x) for x in anvil.server.call('get_accounts', filters={}) ]
+  account_list = [ Account(account_json=x) for x in [] ] #anvil.server.call('get_accounts', filters={}) ]
   ACCOUNTS_D = Accounts(account_list=account_list)
   FIN_YEARS = list(map(lambda x: ( "FY{0}".format(x.year % 100), "FY{0}".format(x.year % 100)), anvil.server.call("get_fy_selections")) )
-  IMPORTERS = anvil.server.call("get_importers")
+  IMPORTERS = [] #anvil.server.call("get_importers")
   ICONS.load()
 
 #####################################################################
@@ -733,18 +734,18 @@ def refresh():
 def refresh_organisations():
   """ Returns a simple list """
   global ORGANISATIONS
-  ORGANISATIONS = anvil.server.call("get_organisations")
+  ORGANISATIONS = [] #anvil.server.call("get_organisations")
   ORGANISATIONS.sort()
   return ORGANISATIONS
 
 def refresh_sources():
   global SOURCES
-  SOURCES = anvil.server.call("get_sources")
+  SOURCES = [] #anvil.server.call("get_sources")
   return SOURCES
   
 def refresh_rules():
   global RULES
-  rule_list = anvil.server.call('get_all_rules')
+  rule_list = [] #anvil.server.call('get_all_rules')
   RULES = Rules(rule_list=rule_list)
   return RULES
   
@@ -756,7 +757,7 @@ def refresh_rules():
 def get_budget_entries(fin_year, refresh=False):
   budget = BUDGETS.get(fin_year)
   if budget is None or refresh:
-    entries_json = anvil.server.call('get_budget_entries', fin_year)
+    entries_json = {} #anvil.server.call('get_budget_entries', fin_year)
     
     # TODO: check that a empty dict is returned when there is no Budget
     entry_list = [ Entry(entry_json={'account_name': k, 'amount': v }) for k,v in entries_json.items() ]
@@ -778,10 +779,10 @@ def get_icon(icon_id):
 
 
 def gpt_set_account_data(account_name):
-  return anvil.server.call('gpt_set_account_data', account_name=account_name)
+  return None #anvil.server.call('gpt_set_account_data', account_name=account_name)
 
 def gpt_run(prompt):
-  return anvil.server.call('gpt_run', prompt=prompt)
+  return None #anvil.server.call('gpt_run', prompt=prompt)
 
 refresh()
 refresh_organisations()
