401f21c2d53484be4d24d5f442bbe8365321d249
simon.e.page@gmail.com <simon.e.page@gmail.com>
2024-05-24 07:21:39 +0000

    Edited Tickets.Transaction.TransactionEntries

--- client_code/Tickets/Transaction/TransactionEntries/__init__.py
+++ client_code/Tickets/Transaction/TransactionEntries/__init__.py
@@ -13,27 +13,6 @@ class TransactionEntries(TransactionEntriesTemplate):
     self.current_year = Data.CURRENT_YEAR
     self.budget_year = Data.BUDGET_YEAR
     
-    self.entries = {
-      2025: { 'Budget': [ 10 ] * 12 }
-    }
-    self.columns = [ 'Month', '2025', '2024', '2023']
-    self.totals = { 'Month': 'Total', '2025': 120, '2024': 144, '2023': 14*12 }
-    self.table_data = [ 
-      {'Month': 'Jul', '2025': 10000, '2024': 120000, '2023': 1400 },
-      {'Month': 'Aug', '2025': 10, '2024': 12, '2023': 14 },
-      {'Month': 'Sep', '2025': 10, '2024': 12, '2023': 14 },
-      {'Month': 'Oct', '2025': 10, '2024': 12, '2023': 14 },
-      {'Month': 'Nov', '2025': 10, '2024': 12, '2023': 14 },
-      {'Month': 'Dec', '2025': 10, '2024': 12, '2023': 14 },
-      {'Month': 'Jan', '2025': 10, '2024': 12, '2023': 14 },
-      {'Month': 'Feb', '2025': 10, '2024': 12, '2023': 14 },
-      {'Month': 'Mar', '2025': 10, '2024': 12, '2023': 14 },
-      {'Month': 'Apr', '2025': 10, '2024': 12, '2023': 14 },
-      {'Month': 'May', '2025': 10, '2024': 12, '2023': 14 },
-      {'Month': 'Jun', '2025': 10, '2024': 12, '2023': 14 },
-    ]
-    
-    self.labels = { 'Jul', 'Aug', 'Sep', 'Oct', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun' }
     self.init_components(**properties)
     # Any code you write here will run before the form opens.
 
@@ -48,7 +27,7 @@ class TransactionEntries(TransactionEntriesTemplate):
       css_class=["table-striped", "table-bordered", "table-condensed"]
     )
 
-    def format_row(cell, **kwargs):
+    def format_column(cell, **kwargs):
       val = cell.getValue()
       if str(val).isnumeric():
         return "{:,.0f}".format(val)
@@ -60,9 +39,14 @@ class TransactionEntries(TransactionEntriesTemplate):
 
     t.data = self.t_data['data']
     t.data.append(self.t_data['totals'])
+
+    month_col = [ {"title":x, "field":x, "width":100, "formatter": format_column, 'headerSort': False,  } 
+                  for x in self.t_data['columns'][0] ]
     
-    t.columns = [ {"title":x, "field":x, "width":100, "formatter": format_row } 
-                  for x in self.t_data['columns'] ]
+    fy_columns = [ {"title":x, "field":x, "width":100, "formatter": format_column, 'headerSort': False,  } 
+                  for x in self.t_data['columns'][1:] ]
+    
+    t.columns = month_col + fy_columns
 
   def update_button_click(self, **event_args):
     """This method is called when the button is clicked"""
