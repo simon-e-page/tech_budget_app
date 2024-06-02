a28d34921cbb9a427ffeb91a7925af93b12c6aac
simon.e.page@gmail.com <simon.e.page@gmail.com>
2024-06-02 02:21:45 +0000

    Created Users.Users.NewUser; Edited Users.Users

---
+++ client_code/Users/Users/NewUser/__init__.py
@@ -0,0 +1,37 @@
+from ._anvil_designer import NewUserTemplate
+from anvil import *
+import anvil.server
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+from ... import Data
+
+class NewUser(NewUserTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.user = {}
+    self.can_save = False
+    self.roles = Data.roles
+    self.users = Data.users
+    
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def email_textbox_change(self, **event_args):
+    """This method is called when the text in this text box is edited"""
+    new_email = self.email_textbox.text
+    if self.users.get(new_email) is not None:
+      alert("User already exists!")
+    else:
+      self.test_save()
+
+  def save_user_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    print(self.user)
+
+  def test_save(self):
+    self.can_save = self.user.get('email', None) and self.user.get('role_name', None)
+      
