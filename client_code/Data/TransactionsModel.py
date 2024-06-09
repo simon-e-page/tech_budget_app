518a4dd16bf37231248e8a6c5eb75055ab9dcb53
simon.e.page@gmail.com <simon.e.page@gmail.com>
2024-06-09 08:38:24 +0000

    Edited Data.TransactionsModel

--- client_code/Data/TransactionsModel.py
+++ client_code/Data/TransactionsModel.py
@@ -166,18 +166,22 @@ class LazyTransactionList(AttributeToDict):
     return count
 
   def search(self, **kwargs):
-    print(kwargs)
+    #print(kwargs)
     trans = []
     for t_id, t in self.__d__.items():
       found = True
-      print("Looing")
       for k,v in kwargs.items():
-        if k=='vendor_name' and t.vendor.vendor_name != v:
-          found = False
-        elif k=='vendor_id' and t.vendor.vendor_id != v:
-          found = False
-        elif t.get(k, None) != v:
-          found = False
+        if k=='vendor_name':
+          found = (t.vendor.vendor_name == v)
+          print(f"{t.vendor.vendor_name} != {v}")
+          #found = False
+        elif k=='vendor_id':
+          found = (t.vendor.vendor_id == v)
+          print(f"{t.vendor.vendor_id} != {v}")
+          #found = False
+        else:
+          found = (t.get(k, None) == v)
+          print(f"{t[k]} != {v}")
       if found:
         trans.append(t)
     return trans
