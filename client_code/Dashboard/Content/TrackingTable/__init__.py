4dbcd0aa53458306da3d2d4b80832950510da433
simon.e.page@gmail.com <simon.e.page@gmail.com>
2024-06-23 06:58:52 +0000

    Edited Dashboard.Content.TrackingTable

--- client_code/Dashboard/Content/TrackingTable/__init__.py
+++ client_code/Dashboard/Content/TrackingTable/__init__.py
@@ -27,6 +27,16 @@ class TrackingTable(TrackingTableTemplate):
       #"pagination_size": 10,
     }
 
+    self.summary_table.options = {
+      "index": "id",  # or set the index property here
+      "css_class": ["table-striped", "table-bordered", "table-condensed"],
+      'pagination': False,
+      'frozenRows': 3,
+      'height': '10vh',
+      #'autoResize': False,
+      #"pagination_size": 10,
+    }
+    
     self.colors = {
       'Actual': {'backgroundColor': '#ffffcc', 'color': 'black' },
       'Forecast': {'backgroundColor': '#ccffcc', 'color': 'black' },
@@ -45,145 +55,244 @@ class TrackingTable(TrackingTableTemplate):
     #self.ly_data = d['ly_data']
     #self.b_data =  d['b_data']
     self.loaded = True
+    self.summary_table_table_built()
     self.tracking_table_table_built()
+
+  def summary_table_table_built(self, **event_args):
+    if not self.loaded:
+      return
+
+    columns = [
+      { 
+        'title': 'Summary',
+        'field': 'id',
+        "hozAlign": "left",
+        "width": 120
+      },
+    ]
     
+    for c in self.year_months:
+      transaction_type = self.transaction_types[c]
+      columns.append({
+        "title": c, 
+        "field": c, 
+        "formatter": self.format_summary, 
+        "hozAlign": "right",
+        "formatterParams": { 'year_month': c, 'backgroundColor': self.colors[transaction_type]['backgroundColor'], 'color': self.colors[transaction_type]['color']},
+        "width": 85
+      })
+
+    columns.append([{
+        "title": "Total", 
+        "field": 'total', 
+        "formatter": self.format_summary, 
+        "width": 100,
+        "headerFilter": "number",
+        "hozAlign": 'right',
+      }])
+
+    self.summary_table.columns = columns
+    a_data = { 'id': 'CY Actuals / Forecast', 'total': 0.0 } | { x: 0.0 for x in self.year_months }
+    ly_data = { 'id': 'LY Actuals', 'total': 0.0 } | { x: 0.0 for x in self.year_months }
+    d_data = { 'id': 'Difference', 'total': 0.0 } | { x: 0.0 for x in self.year_months }
+    p_data = { 'id': 'Percentage', 'total': 0.0 } | { x: 0.0 for x in self.year_months }
+    ac_data = { 'id': 'CY Cumulative', 'total': 0.0 } | { x: 0.0 for x in self.year_months }
+    lc_data = { 'id': 'LY Cumulative', 'total': 0.0 } | { x: 0.0 for x in self.year_months }
+    dc_data = { 'id': 'Cum Difference', 'total': 0.0 } | { x: 0.0 for x in self.year_months }
+    pc_data = { 'id': 'Cum Percentage', 'total': 0.0 } | { x: 0.0 for x in self.year_months }
+    
+    for row in self.data:
+      for year_month in self.year_months:
+        a_data[year_month] += row[year_month]
+        ly_data[year_month] += row[f"{year_month}LY"]
+
+      for i, year_month in enumerate(self.year_months):
+        d_data[year_month] += a_data[year_month] - ly_data[year_month]
+        try:
+          p_data[year_month] = int(d_data[year_month] / ly_data[year_month] * 100)
+        except Exception:
+          p_data[year_month] = 0
+
+        ac_data[year_month] = a_data[year_month] + a_data[self.year_months[i-1]] if i>0 else 0
+        lc_data[year_month] = ly_data[year_month] + ly_data[self.year_months[i-1]] if i>0 else 0
+        dc_data[year_month] = ac_data[year_month] - lc_data[year_month]
+        try:
+          pc_data[year_month] = int(dc_data[year_month] / lc_data[year_month] * 100)
+        except Exception:
+          pc_data[year_month] = 0
+
+    self.summary_table.data = [ a_data, ly_data, d_data, p_data, ac_data, lc_data, dc_data, pc_data ]
+      
+    
+  
+  # Vendor Formatter
+  def vendor_formatter(self, cell, **params):
+    vendor_id = cell.getData()['vendor_id']
+    vendor = self.vendors.get(vendor_id)
+
+    def open_vendor(sender, **event_args):
+      print("Opening vendor: {0}".format(sender.tag.vendor_name))
+      ret = alert(Vendor(item=sender.tag, show_save=False), large=True, title="Vendor Details", buttons=[ ('Save Changes', True), ('Cancel', False) ])
+      if ret:
+        try:
+          vendor.update()
+        except Exception as e:
+          print("Failed to update Vendor!")
+      return
+
+    link = Link(text=cell.get_value(), tag=vendor)
+    link.set_event_handler("click", open_vendor)
+    return link
+
+  # Summary formatter
+  def format_summary(self, cell, **params):
+    val = cell.getValue()
+    data = cell.get_data()
+    
+    if params.get('backgroundColor', None):
+      cell.getElement().style.backgroundColor = params['backgroundColor']
+    if params.get('color', None):
+      cell.getElement().style.color = params['color']
+
+    if "Percentage" in data['id']:
+      if val > 0:
+        text = "+{:,.0f}%".format(val)
+      elif val < 0:
+        text = "{:,.0f}%".format(val)
+      else:
+        text = '-'
+      val = Label(text = text,
+                  align='right',
+                  foreground=params['color'], 
+                  background=params['backgroundColor']
+                  )
+    else:
+      val = Label(text = "{:,.0f}".format(FinancialNumber(val)),
+                  align='right',
+                  foreground=params['color'], 
+                  background=params['backgroundColor']
+                  )
+    return val
+  
+  # Entry formatter
+  def format_entry(self, cell, **params):
+    val = cell.getValue()
+    data = cell.get_data()
+    ym = params.get('year_month')
+    #b_ym = f"{ym}B"
+    ly_ym = f"{ym}LY"
+    
+    if params.get('backgroundColor', None):
+      cell.getElement().style.backgroundColor = params['backgroundColor']
+    if params.get('color', None):
+      cell.getElement().style.color = params['color']
+
+    try:
+      ly_delta = (int(val) - int(data[ly_ym]))/int(data[ly_ym])
+      ly_delta = int(ly_delta * 100)
+    except Exception:
+      ly_delta = "INF"
+      
+    tooltip = f"LY: {FinancialNumber(data[ly_ym]):,.0f}"
+
+    if int(val) < int(data[ly_ym]):
+      icon = "fa:arrow-down"
+      tooltip += f"\n{ly_delta}%"
+    elif int(val) > int(data[ly_ym]):
+      icon = "fa:arrow-up"
+      tooltip += f"\n+{ly_delta}%"
+    else:
+      icon = None
+      tooltip = None
+      
+    try:
+      val = Label(text = "{:,.0f}".format(FinancialNumber(val)),
+                  icon=icon, 
+                  tooltip=tooltip, 
+                  align='right',
+                  icon_align="left", 
+                  foreground=params['color'], 
+                  background=params['backgroundColor']
+                  )
+      #val = "{:,.0f}".format(val)
+    except Exception:
+      print("Entry Exception!")
+      pass
+    return val
+  
+  
+  # Total formatter
+  def format_total(self, cell, **params):
+    val = cell.get_value()
+    ly_total = cell.get_data()['totalLY']
+
+    try:
+      ly_delta = (int(val) - int(ly_total))/int(ly_total)
+      ly_delta = int(ly_delta * 100)
+    except Exception:
+      ly_delta = 'INF'
+    
+    cell.getElement().style.color = 'white'
+    tooltip = f"LY: {FinancialNumber(ly_total):,.0f}"
+    
+    if int(val) < int(ly_total):
+      background_color = '#043d1b'
+      tooltip += f"\n{ly_delta}%"
+    elif int(val) > int(ly_total):                  
+      background_color = '#4d0404'
+      tooltip += f"\n+{ly_delta}%"
+    else:
+      background_color = '#424140'
+
+    cell.getElement().style.backgroundColor = background_color
+
+    try:
+      val = Label(text = "{:,.0f}".format(FinancialNumber(val)),
+                  bold = True,
+                  align='right',
+                  icon_align="left", 
+                  foreground='white', 
+                  tooltip=tooltip,
+                  background=background_color
+                  )
+      #val = "{:,.0f}".format(val)
+    except Exception:
+      print("Total Exception!")
+      val = 'NA'
+    return val
+
+  # Change formatter
+  def format_percent(self, cell, **params):
+    val = cell.get_value()
+    
+    cell.getElement().style.backgroundColor = '#0d6e12' if val <= 0 else '#6e0d15'
+    cell.getElement().style.color = 'white'
+    
+    try:
+      if val > 0:
+        val = "+{:,.0f}%".format(val)
+      elif val < 0:
+        val = "-{:,.0f}%".format(val)
+      else:
+        val = ''
+    except Exception:
+      print("Percent Exception!")
+      val = 'NA'
+    return val
+  
+  
   def tracking_table_table_built(self, **event_args):
     """This method is called when the tabulator instance has been built - it is safe to call tabulator methods"""
     if not self.loaded:
       return
-      
-    # Vendor Formatter
-    def vendor_formatter(cell, **params):
-      vendor_id = cell.getData()['vendor_id']
-      vendor = self.vendors.get(vendor_id)
-
-      def open_vendor(sender, **event_args):
-        print("Opening vendor: {0}".format(sender.tag.vendor_name))
-        ret = alert(Vendor(item=sender.tag, show_save=False), large=True, title="Vendor Details", buttons=[ ('Save Changes', True), ('Cancel', False) ])
-        if ret:
-          try:
-            vendor.update()
-          except Exception as e:
-            print("Failed to update Vendor!")
-        return
-
-      link = Link(text=cell.get_value(), tag=vendor)
-      link.set_event_handler("click", open_vendor)
-      return link
-
-    # Entry formatter
-    def format_entry(cell, **params):
-      val = cell.getValue()
-      data = cell.get_data()
-      ym = params.get('year_month')
-      #b_ym = f"{ym}B"
-      ly_ym = f"{ym}LY"
-      
-      if params.get('backgroundColor', None):
-        cell.getElement().style.backgroundColor = params['backgroundColor']
-      if params.get('color', None):
-        cell.getElement().style.color = params['color']
-
-      try:
-        ly_delta = (int(val) - int(data[ly_ym]))/int(data[ly_ym])
-        ly_delta = int(ly_delta * 100)
-      except Exception:
-        ly_delta = "INF"
-        
-      tooltip = f"LY: {FinancialNumber(data[ly_ym]):,.0f}"
-
-      if int(val) < int(data[ly_ym]):
-        icon = "fa:arrow-down"
-        tooltip += f"\n{ly_delta}%"
-      elif int(val) > int(data[ly_ym]):
-        icon = "fa:arrow-up"
-        tooltip += f"\n+{ly_delta}%"
-      else:
-        icon = None
-        tooltip = None
-        
-      try:
-        val = Label(text = "{:,.0f}".format(FinancialNumber(val)),
-                    icon=icon, 
-                    tooltip=tooltip, 
-                    align='right',
-                    icon_align="left", 
-                    foreground=params['color'], 
-                    background=params['backgroundColor']
-                   )
-        #val = "{:,.0f}".format(val)
-      except Exception:
-        print("Entry Exception!")
-        pass
-      return val
-
-    # Total formatter
-    def format_total(cell, **params):
-      val = cell.get_value()
-      ly_total = cell.get_data()['totalLY']
-
-      try:
-        ly_delta = (int(val) - int(ly_total))/int(ly_total)
-        ly_delta = int(ly_delta * 100)
-      except Exception:
-        ly_delta = 'INF'
-      
-      cell.getElement().style.color = 'white'
-      tooltip = f"LY: {FinancialNumber(ly_total):,.0f}"
-      
-      if int(val) < int(ly_total):
-        background_color = '#043d1b'
-        tooltip += f"\n{ly_delta}%"
-      elif int(val) > int(ly_total):                  
-        background_color = '#4d0404'
-        tooltip += f"\n+{ly_delta}%"
-      else:
-        background_color = '#424140'
-
-      cell.getElement().style.backgroundColor = background_color
-
-      try:
-        val = Label(text = "{:,.0f}".format(FinancialNumber(val)),
-                    bold = True,
-                    align='right',
-                    icon_align="left", 
-                    foreground='white', 
-                    tooltip=tooltip,
-                    background=background_color
-                   )
-        #val = "{:,.0f}".format(val)
-      except Exception:
-        print("Total Exception!")
-        val = 'NA'
-      return val
-
-    # Change formatter
-    def format_percent(cell, **params):
-      val = cell.get_value()
-      
-      cell.getElement().style.backgroundColor = '#0d6e12' if val <= 0 else '#6e0d15'
-      cell.getElement().style.color = 'white'
-      
-      try:
-        if val > 0:
-          val = "+{:,.0f}%".format(val)
-        elif val < 0:
-          val = "-{:,.0f}%".format(val)
-        else:
-          val = ''
-      except Exception:
-        print("Percent Exception!")
-        val = 'NA'
-      return val
-
-    
+          
     columns = [{
         "title": "Vendor",
         "field": "vendor_name",
         'width': 250,
         "headerFilter": "input",
         "headerFilterFunc": "starts",
-        'formatter': vendor_formatter      
+        'formatter': self.vendor_formatter      
     }]
 
     for c in self.year_months:
@@ -191,7 +300,7 @@ class TrackingTable(TrackingTableTemplate):
       columns.append({
         "title": c, 
         "field": c, 
-        "formatter": format_entry, 
+        "formatter": self.format_entry, 
         "hozAlign": "right",
         "formatterParams": { 'year_month': c, 'backgroundColor': self.colors[transaction_type]['backgroundColor'], 'color': self.colors[transaction_type]['color']},
         "width": 85,
@@ -201,7 +310,7 @@ class TrackingTable(TrackingTableTemplate):
     columns.append({
         "title": "Total", 
         "field": 'total', 
-        "formatter": format_total, 
+        "formatter": self.format_total, 
         "width": 100,
         "headerFilter": "number",
         "hozAlign": 'right',
@@ -210,7 +319,7 @@ class TrackingTable(TrackingTableTemplate):
     change_col = {
           "title": "Change", 
           "field": 'change', 
-          "formatter": format_percent, 
+          "formatter": self.format_percent, 
           "width": 85,
           "hozAlign": 'right',
     }
@@ -231,6 +340,9 @@ class TrackingTable(TrackingTableTemplate):
     #self.tracking_table.data = self.data
     self.tracking_table.data = compare(c_data)
 
+
+
+  
   def yes_compare(self, c_data):
     new_data = []
     for i, row in enumerate(self.data):
