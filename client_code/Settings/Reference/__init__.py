759de47d5889c8439de014613f1728cb1a7a89f7
simon.e.page@gmail.com <simon.e.page@gmail.com>
2024-08-30 07:05:20 +0000

    Edited Settings.Reference

--- client_code/Settings/Reference/__init__.py
+++ client_code/Settings/Reference/__init__.py
@@ -13,6 +13,14 @@ class Reference(ReferenceTemplate):
     self.attribute_name = None
     self.attribute_data = None
     self.attribute_names = Data.get_attribute_names()
+
+    self.attribute_table.options = {
+      "index": "value",  # or set the index property here
+      "selectable": "highlight",
+      'css_class': ["table-striped", "table-bordered", "table-condensed"],
+      'pagination_size': 10
+    }
+
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
@@ -44,7 +52,32 @@ class Reference(ReferenceTemplate):
       else:
         link = Label(text='')
       return link
+
+    def replace_formatter(cell, **params):
+      tag = cell.getData()['value']
+      count = cell.getData()['used']
       
+      def replace_value(sender, **event_args):
+        old_value = sender.tag
+        new_values = [ x['value'] for x in self.attribute_data if x['value'] != old_value ]
+        replacement_dropdown, replacement_form = self.get_replacement_form(old_value, new_values)
+        if alert(replacement_form):
+          new_value = replacement_dropdown.selected_value
+          print(f"Got new value: {new_value}")
+          ret = Data.replace_attribute(attribute_name=self.attribute_name, old_value=old_value, new_value=new_value)
+          if ret:
+            Notification(f"Successfully replaced all instances of {old_value}!").show()
+            
+          #TODO: rebuild table          
+
+      if not count:
+        link = Label(text='')
+      else:
+        link = Link(text="Replace", tag=tag)
+        link.set_event_handler('click', replace_value)
+      return link
+
+    
     columns = [
       {
         "title": "",
@@ -63,8 +96,15 @@ class Reference(ReferenceTemplate):
         "title": "Currently Used?",
         "field": "used",
         "formatter": "tickCross",
-        'width': 50,
-      }
+        'width': 150,
+      },
+      {
+        "title": "Replace",
+        "field": "replace",
+        "formatter": replace_formatter,
+        'width': 150,
+      },
+      
         ]
 
     self.attribute_data = Data.get_attributes(self.attribute_name, with_count=True)[self.attribute_name]
@@ -84,3 +124,11 @@ class Reference(ReferenceTemplate):
   def attribute_value_change(self, **event_args):
     """This method is called when the text in this text box is edited"""
     self.refresh_data_bindings()
+
+  def get_replacement_form(self, old_value, new_values):
+    panel = FlowPanel()
+    replacement_dropdown = DropDown(items=new_values, include_placeholder=True, placeholder='Select new value')
+    label = Label(text=f"Choose a new value to replace {old_value}:")
+    panel.add_component(label)
+    panel.add_component(replacement_dropdown)
+    return replacement_dropdown, panel
