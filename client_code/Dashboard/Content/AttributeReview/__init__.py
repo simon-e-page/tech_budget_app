ebee4390ea59f2ec213559da1c4ecee9066af005
simon.e.page@gmail.com <simon.e.page@gmail.com>
2024-09-09 01:10:52 +0000

    Edited Dashboard.Content.AttributeReview

--- client_code/Dashboard/Content/AttributeReview/__init__.py
+++ client_code/Dashboard/Content/AttributeReview/__init__.py
@@ -12,6 +12,7 @@ class AttributeReview(AttributeReviewTemplate):
   def __init__(self, **properties):
     self.orig_set = {}
     self.review_set = {}
+    self.new_set = {}
     
     self.vendor_list = []
     self.attribute_list = []
@@ -56,24 +57,27 @@ class AttributeReview(AttributeReviewTemplate):
   
   def vendor_dropdown_change(self, **event_args):
     """This method is called when an item is selected"""
-    self.value_label_text = "Values:"
-    self.value_table.visible = False
-    
-    if self.selected_vendor is not None:
-      cost_centre = self.orig_set[self.selected_vendor]['cost_centre']
-      self.attribute_list = sorted(x for x in self.orig_set[self.selected_vendor].keys() if x not in ['cost_centre', 'forecast_ids', 'actual_ids'])
-      self.review_set[self.selected_vendor] = self.review_set.get(self.selected_vendor, {}) 
-
-      self.render_value_table(self.cost_centre_table, cost_centre, edit=False)
-      self.attribute_label_text = f"Attributes for {self.selected_vendor}"
-      self.refresh_data_bindings()
+    if not self.vendor_changed or confirm("Changed will be lost?"):
+      self.value_label_text = "Values:"
+      self.value_table.visible = False
+      self.vendor_changed = False
       
-    else:
-      self.value_list = []
-      self.attribute_list = []
-      self.attribute_label_text = "Attributes:"
-      self.cost_centre_table.visible = False
-      self.refresh_data_bindings()
+      if self.selected_vendor is not None:
+        cost_centre = self.orig_set[self.selected_vendor]['cost_centre']
+        self.attribute_list = sorted(x for x in self.orig_set[self.selected_vendor].keys() if x not in ['cost_centre', 'forecast_ids', 'actual_ids'])
+        self.review_set[self.selected_vendor] = self.review_set.get(self.selected_vendor, {}) 
+        self.new_set[self.selected_vendor] = self.new_set.get(self.selected_vendor, {}) 
+  
+        self.render_value_table(self.cost_centre_table, cost_centre, edit=False)
+        self.attribute_label_text = f"Attributes for {self.selected_vendor}"
+        self.refresh_data_bindings()
+        
+      else:
+        self.value_list = []
+        self.attribute_list = []
+        self.attribute_label_text = "Attributes:"
+        self.cost_centre_table.visible = False
+        self.refresh_data_bindings()
 
 
   
@@ -83,9 +87,24 @@ class AttributeReview(AttributeReviewTemplate):
   def attribute_dropdown_change(self, **event_args):
     """This method is called when an item is selected"""
     if self.selected_attribute is not None:
-      value_splits = self.orig_set[self.selected_vendor][self.selected_attribute]
-      value_list = { x: value_splits.get(x, [0.0, 0.0]) for x in Data.REFS[self.selected_attribute] }
-      self.review_set[self.selected_vendor][self.selected_attribute] = value_list
+      if self.selected_attribute in self.new_set[self.selected_vendor]:
+        review_list = self.review_set[self.selected_vendor][self.selected_attribute]
+        value_list = self.new_set[self.selected_vendor][self.selected_attribute]
+      else:
+        value_splits = self.orig_set[self.selected_vendor][self.selected_attribute]
+        temp_list = { x: value_splits.get(x, [0.0, 0.0]) for x in Data.REFS[self.selected_attribute] }
+        forecast_total = sum(x[0] for x in temp_list.values())
+        actual_total = sum(x[1] for x in temp_list.values())
+
+        value_list = { x: [
+          round(y[0] / forecast_total, 2) if forecast_total != 0 else 0,
+          round(y[1] / actual_total, 2) if actual_total != 0 else 0,
+        ] for x,y in temp_list.keys() }
+
+        review_list = value_list.copy()
+        
+        self.review_set[self.selected_vendor][self.selected_attribute] = review_list
+        self.new_set[self.selected_vendor][self.selected_attribute] = value_list
       
       if len(value_splits) > 0:
         self.value_label_text = f"Forecast has {len(value_splits)} values for {self.selected_attribute}. Confirm or change splits:"
@@ -93,7 +112,7 @@ class AttributeReview(AttributeReviewTemplate):
         self.value_label_text = f"Forecast has no values for {self.selected_attribute}. Select one or more splits:"
 
       self.value_label.visible = True
-      self.render_value_table(self.value_table, value_list)
+      self.render_value_table(self.value_table, value_list, old_values=review_list)
       self.refresh_data_bindings()
     else:
       self.value_table.visible = False
@@ -102,12 +121,16 @@ class AttributeReview(AttributeReviewTemplate):
     
 
   
-  def render_value_table(self, table, values, edit=True):
+  def render_value_table(self, table, values, old_values={}, edit=True):
     
     def percent_formatter(cell, **params):
       val = cell.get_value()
-      val = "{:.1%}".format(val)
-      return val
+      field = cell.get_field()
+      old_value = cell.get_data.get(f"old_{field}", None)
+      if old_value is not None and old_value != val:
+        cell.getElement().style.backgroundColor = "yellow"
+      formatted_value = "{:.1%}".format(val)
+      return formatted_value
 
     
     columns = [
@@ -134,80 +157,78 @@ class AttributeReview(AttributeReviewTemplate):
       }
     ]
 
-    #print(values)
-    forecast_total = sum(x[0] for x in values.values())
-    actual_total = sum(x[1] for x in values.values())
     
     data = [ { 
       'value': x,
-      'forecast_amount': y[0],
-      'actual_amount': y[1],
-      'forecast_percent': round(y[0] / forecast_total, 2) if forecast_total != 0 else 0,
-      'actual_percent': round(y[1] / actual_total, 2) if actual_total != 0 else 0
+      'forecast_percent': y[0],
+      'actual_percent': y[1],
+      'old_forecast_pecent': old_values.get(x, [None, None])[0],
+      'old_actual_pecent': old_values.get(x, [None, None])[1],
     } for x,y in values.items() ]
 
     forecast_percent_total = sum(x['forecast_percent'] for x in data)
     actual_percent_total = sum(x['actual_percent'] for x in data)
     self.error_label.visible = (forecast_percent_total != 1) or (actual_percent_total != 1)
-      
+
+    
     def table_cell_edited(cell, **event_args):
       """This method is called when a cell is edited"""
-      new_data = cell.get_data()
+      this_row = cell.get_data()
       field = cell.getField()
-      value = new_data['value']
-      total = 0.0
-      for row in data:
-        if row['value'] == value:
-          row[field] = round(cell.get_value(), 2)
-        total += row[field]
-  
-      self.error_label.visible = (total != 1.00) 
+      value = this_row['value']
+      new_percents = [this_row['format_percent'], this_row['actual_percent']]      
+      self.new_set[self.selected_vendor][self.selected_attribute][value] = new_percents
+
+      index = 0 if field == 'forecast_percent' else 1
+      total = sum(x[index] for x in self.new_set[self.selected_vendor][self.selected_attribute])
+      self.error_label.visible = (total != 1.00)
+      self.review_changed()
       self.refresh_data_bindings()
+
     
     table.columns = columns
     table.data = data
     table.add_event_handler("cell_edited", table_cell_edited)
     table.visible = True
+    self.review_changed()
     self.refresh_data_bindings()
 
-  def review_changes(self, new_data):
-    totals_ok = False
-    altered = False
-    
-    for attribute, old_data in self.review_set[self.selected_vendor].items():
-      for row in old_data:
-        pass
-        
-      
-  
+              
   def get_splits(self, field):
     splits = { x['value']: x[field] for x in self.value_table.data if x[field] != 0 }
     return splits
-    
-  def apply_forecast_button_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    splits = self.get_splits('forecast_percent')
-    transaction_ids = self.review_set[self.selected_vendor]['forecast_ids']
-    message = f"Setting {self.selected_attribute} for {self.selected_vendor} to {splits} for all Forecast Lines!"
-    if self.update(transaction_ids, self.selected_attribute, splits, apply_to='Forecast'):
-      Notification(message=message, title="Update successful!").show()
-    else:
-      alert("Update failed - check logs!")
-      
+
+  
+  def review_changed(self):
+    review_list = self.review_set[self.selected_vendor]
+    new_list = self.new_set[self.selected_vendor]
+    changed = False
+    for attribute, values in new_list.keys():
+      diff = sum(y - review_list[attribute][x] for x, y in values.items())
+      changed = changed or (diff != 0)
+
+    self.vendor_changed = changed
+
+        
 
   def apply_actuals_button_click(self, **event_args):
     """This method is called when the button is clicked"""
-    splits = self.get_splits('actual_percent')
-    transaction_ids = self.review_set[self.selected_vendor]['actual_ids']
-    message = f"Set {self.selected_attribute} for {self.selected_vendor} to {splits} for all Actual Lines!"    
-    if self.update(transaction_ids, self.selected_attribute, splits, apply_to='Actuals'):
+    splits = self.new_set[self.selected_vendor]
+    forecast_ids = self.review_set[self.selected_vendor]['forecast_ids']
+    actual_ids = self.review_set[self.selected_vendor]['actual_ids']
+    
+    message = f"Update {self.selected_vendor} to {splits} for all Forecast and Actual Lines!"    
+    
+    if self.update(forecast_ids, actual_ids, splits):
       Notification(message=message, title="Update successful!").show()
+      self.vendor_changed = False
     else:
       alert("Update failed - check logs!")
       
 
-  def update(self, transaction_ids, field, splits, apply_to):
-    return Data.apply_attribute_splits(transaction_ids, field, splits, apply_to)
+  def update(self, forecast_ids, actual_ids, splits):
+    return True
+    #return Data.apply_attribute_splits(forecast_ids, actual_ids, splits)
   
     
     
