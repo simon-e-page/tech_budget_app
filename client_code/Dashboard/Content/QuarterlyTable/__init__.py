a9ebacc1f4842f52f5b5a8c84a24c112a077ffe1
simon.e.page@gmail.com <simon.e.page@gmail.com>
2024-10-16 06:23:44 +0000

    Created Dashboard.Content.QuarterlyTable

---
+++ client_code/Dashboard/Content/QuarterlyTable/__init__.py
@@ -0,0 +1,18 @@
+from ._anvil_designer import QuarterlyTableTemplate
+from anvil import *
+import anvil.server
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class QuarterlyTable(QuarterlyTableTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def build_table(self):
+    pass
